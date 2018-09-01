f97d045597ad5f859f40c323352f309ea9a0fb50
vitorgsevero <vitorgsevero@gmail.com>
2018-09-01 14:25:13 -0300

    Adicionando exercícios

---
+++ src/aula02/Pilha.java
@@ -0,0 +1,44 @@
+package aula02;
+public class Pilha {
+	
+	private int valores[];
+	private int topo;
+	
+	
+	public Pilha(){ // Construtor
+		
+		this.valores = new int[10];
+		this.topo = -1; 
+		
+	}
+	
+	public void push(int elemento){ // Empilhar
+		
+		this.topo++; //Avança uma posição para que a anterior seja ocupada pelo elemento
+		this.valores[topo] = elemento;
+			
+	}
+	
+	public boolean isEmpty(){ // Verifica se está vazia
+		
+		return (this.topo == -1);
+		
+	}
+	
+	public boolean isFull(){ // Verifica se está cheia
+		
+		return (this.topo == 9);
+	}
+	
+	public int pop(){ // Desempilhar
+		
+		int elemento = valores[topo];
+		topo--;
+		return elemento;
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/aula5/IPilha.java
@@ -0,0 +1,7 @@
+package aula5;
+
+public interface IPilha {
+	public void push(int elemento);
+	public int pop();
+	public boolean isEmpty();
+}
